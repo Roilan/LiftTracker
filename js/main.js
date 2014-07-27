cf309adf298facbf764315e2547aaab655aca9db
Roilan <roilan14@gmail.com>
2014-07-27 19:44:38 -0400

    Moved function outside of form submission handler

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
+	var $name = $('#name').val();
+	var $sets = $('#sets').val();
+	var $reps = $('#reps').val();
+	var $weight = $('#weight').val();
+
+	function addExercise() {
+		if ($name && $sets && $reps && $weight != '') {
+			// Adds input to list item
+			$('.nameList').append('<li>' + $name + '</li>');
+			$('.setList').append('<li>' + $sets + '</li>');
+			$('.repsList').append('<li>' + $reps + '</li>');
+			$('.weightList').append('<li>' + $weight + '</li>');
+
+			// Clear input values
+			$('input').val('');			
+		} else {
+			alert('Error, empty fields.');
+		}
+	}
+
 	$('.btnDay').on('click', function() {
 		var setDay = prompt('What exercise day is it?');
 
@@ -7,28 +27,8 @@ $(document).ready(function() {
 	});
 
 	$('form').submit(function(event) {
-		var $name = $('#name').val();
-		var $sets = $('#sets').val();
-		var $reps = $('#reps').val();
-		var $weight = $('#weight').val();
-
-		function addExercise() {
-			if ($name && $sets && $reps && $weight != '') {
-				// Adds input to list item
-				$('.nameList').append('<li>' + $name + '</li>');
-				$('.setList').append('<li>' + $sets + '</li>');
-				$('.repsList').append('<li>' + $reps + '</li>');
-				$('.weightList').append('<li>' + $weight + '</li>');
-
-				// Clear input values
-				$('input').val('');			
-			} else {
-				alert('Error, empty fields.');
-			}
-		}
-		
-		addExercise();
 		event.preventDefault();
+		addExercise();
 	});
 
 });
