eaec4d41ab5fe7a639c9a21d78a6d97c5da23c5b
olivierrr <oliviersinbox@gmail.com>
2014-07-31 16:16:45 -0700

    added input validation and removed connect-flash dependency

--- api/config/passport.js
+++ api/config/passport.js
@@ -1,10 +1,10 @@
 var LocalStrategy   = require('passport-local').Strategy;
 var User       		= require('../app/models/user');
+var validator       = require('validator');
 
 module.exports = function(passport) {
 
-    // passport session setup ==================================================
-
+    // PASSPORT SESSION
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
@@ -15,8 +15,7 @@ module.exports = function(passport) {
         });
     });
 
-    // LOCAL SIGNUP ============================================================
-
+    // LOCAL SIGNUP
     passport.use('local-signup', new LocalStrategy({
         usernameField : 'email',
         passwordField : 'password',
@@ -29,6 +28,10 @@ module.exports = function(passport) {
             if (err) return done(err);
 
             if (user) return done(null, false, { error: 'email is taken' }); //email is already taken
+
+            if (!validator.isEmail(email)) return done(null, false, { error: 'that\'s not an email!'});
+
+            if (!validator.isLength(password, 8, 100)) return done(null, false, { error: 'password must be a minimum of 8 characters'});
 
             else { 
 
--- api/server.js
+++ api/server.js
@@ -1,52 +1,42 @@
-
-// REQUIRE =============================================================================
-
+// REQUIRE
 var express  = require('express');
 var app      = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
 var morgan = require('morgan'); //dev
 
-// DB ==================================================================================
-
+// DB
 var configDB = require('./config/database.js');
+console.log('connecting to database...');
 mongoose.connect(configDB.url, function (err) {
 	if (err) console.log(err);
 	else console.log('connected to database');
 });
 
-// PASSPORT CONFIG =====================================================================
-
+// PASSPORT CONFIG
 require('./config/passport')(passport);
 
-// EXPRESS CONFIG ======================================================================
-
+// EXPRESS CONFIG
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// SESSION CONFIG ======================================================================
-
+// SESSION CONFIG
 app.use(session({ secret: 'placeholder' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
 
-// ROUTES ==============================================================================
-
+// ROUTES
 require('./app/routes.js')(app, passport);
 
-// LAUNCH ==============================================================================
-
+// LAUNCH
 var port = process.env.PORT || 8080;
-
 app.listen(port, function (err) {
 	if (err) console.log(err);
-	else console.log('app live on port: ' + port);
+	else console.log('listening on port: ' + port);
 });
