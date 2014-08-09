7d15cf3484c8648fc6c7747c0b26578e67d2871a
Roilan <roilan14@gmail.com>
2014-08-09 18:05:23 -0400

    Changed email validation on submit and button

--- js/main.js
+++ js/main.js
@@ -31,8 +31,6 @@ $(document).ready(function() {
 		addExercise();
 	});
 
-	var $emailVal = $('.signupEmail').val();
-
 	// Validates email through regular expression
 	function validateEmail($emailVal) {
 		 var regex = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
@@ -47,16 +45,16 @@ $(document).ready(function() {
 	//Sign up Password check
 	// Initial hide and disable on load
 	$('.formSignup span').hide();
-	//$('.btnSignup').prop('disabled', true);
+	$('.btnSignup').prop('disabled', true);
 
 	// Checks if password is 8 characters or longer
 	function passCheck() {
 		if ($(this).val().length >= 8 && $(this).val().length < 101) {
 			$(this).next().hide();
-			//$('.formSignup span').next().prop('disabled', false);
+			$('.formSignup span').next().prop('disabled', false);
 		} else {
 			$(this).next().show();
-			//$('.formSignup span').next().prop('disabled', true);
+			$('.formSignup span').next().prop('disabled', true);
 		}
 	}
 
@@ -67,9 +65,10 @@ $(document).ready(function() {
 	$('.btnSignup').on('click', function(event) {
 		event.preventDefault();
 
-		if (validateEmail($emailVal)) {
-		} else {
-			console.log('Invalid email');
+		var $emailVal = $('.signupEmail').val();
+
+		if (validateEmail($emailVal) == false) {
+			alert('Invalid email');
 		}
 	});
 });
